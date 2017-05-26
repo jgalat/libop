0b21e26a2bd1bf1cb68d9aabe4277347fd05cc05
Jorge Galat <jrgglt@gmail.com>
2017-05-26 11:06:06 -0300

    Implemented greeks on eur_an

--- src/pricing_methods/european/european_analytic.c
+++ src/pricing_methods/european/european_analytic.c
@@ -40,10 +40,10 @@ static int option_price(option_data od, pricing_data pd, double S,
 
   double d1, d2, result;
 
-  d1 = (log(S / K) + (r - d + powf(sigma, 2) / 2) * ttl) / (sigma * sqrt(ttl));
+  d1 = (log(S / K) + (r - d + pow(sigma, 2) / 2) * ttl) / (sigma * sqrt(ttl));
   d2 = d1 - sigma * sqrt(ttl);
 
-  switch(type) {
+  switch (type) {
     case OPTION_CALL:
       result = S * exp(-d * ttl) * cdf(d1) - K * exp(-r * ttl) * cdf(d2);
       break;
@@ -51,6 +51,7 @@ static int option_price(option_data od, pricing_data pd, double S,
       result = K * exp(-r * ttl) * cdf(-d2) - S * exp(-d * ttl) * cdf(-d1);
       break;
     default:
+      result = -1;
       break;
   }
 
@@ -58,6 +59,164 @@ static int option_price(option_data od, pricing_data pd, double S,
   return 0;
 }
 
+static int greek_delta(option_data od, pricing_data pd, double S,
+  date ttl, result ret, void *pm_data) {
+  // exercise_type et = option_get_et(o);
+  /* check if it is eur... etcccc */
+
+  option_type type = od_get_option_type(od);
+  // date maturity = option_get_maturity(o); /* no use here */
+
+  double  sigma = pd_get_volatility(pd),
+          r = pd_get_risk_free_rate(pd),
+          d = pd_get_dividend(pd),
+          K = od_get_strike(od);
+
+  double d1, result;
+
+  d1 = (log(S / K) + (r - d + pow(sigma, 2) / 2) * ttl) / (sigma * sqrt(ttl));
+
+  switch (type) {
+    case OPTION_CALL:
+      result = exp(-d * ttl) * cdf(d1);
+      break;
+    case OPTION_PUT:
+      result = exp(-d * ttl) * (cdf(d1) - 1);
+      break;
+    default:
+      result = -1;
+      break;
+  }
+
+  set_result(ret, result);
+  return 0;
+}
+
+static int greek_gamma(option_data od, pricing_data pd, double S,
+  date ttl, result ret, void *pm_data) {
+  // exercise_type et = option_get_et(o);
+  /* check if it is eur... etcccc */
+
+  // date maturity = option_get_maturity(o); /* no use here */
+
+  double  sigma = pd_get_volatility(pd),
+          r = pd_get_risk_free_rate(pd),
+          d = pd_get_dividend(pd),
+          K = od_get_strike(od);
+
+  double d1, snpd, result;
+
+  d1 = (log(S / K) + (r - d + pow(sigma, 2) / 2) * ttl) / (sigma * sqrt(ttl));
+
+  snpd = (1 / sqrt(2 * M_PI)) * exp(-pow(d1, 2) / 2);
+
+  result = (exp(-d * ttl) / (S * sigma * sqrt(ttl))) * snpd;
+
+  set_result(ret, result);
+  return 0;
+}
+
+static int greek_theta(option_data od, pricing_data pd, double S,
+  date ttl, result ret, void *pm_data) {
+  // exercise_type et = option_get_et(o);
+  /* check if it is eur... etcccc */
+
+  option_type type = od_get_option_type(od);
+  // date maturity = option_get_maturity(o); /* no use here */
+
+  double  sigma = pd_get_volatility(pd),
+          r = pd_get_risk_free_rate(pd),
+          d = pd_get_dividend(pd),
+          K = od_get_strike(od);
+
+  double d1, d2, snpd, result;
+
+  d1 = (log(S / K) + (r - d + pow(sigma, 2) / 2) * ttl) / (sigma * sqrt(ttl));
+  d2 = d1 - sigma * sqrt(ttl);
+
+  snpd = (1 / sqrt(2 * M_PI)) * exp(-pow(d1, 2) / 2);
+
+  double daysperyear = 365.0; /* FIXME */
+
+  switch (type) {
+    case OPTION_CALL:
+      result = (1.0 / daysperyear)
+        * (-( ((S * sigma * exp(-d * ttl)) / (2 * sqrt(ttl))) * snpd )
+          - r * K * exp(-r * ttl) * cdf(d2) + d * S * exp(-d * ttl) * cdf(d1));
+      break;
+    case OPTION_PUT:
+      result = (1.0 / daysperyear)
+        * (-( ((S * sigma * exp(-d * ttl)) / (2 * sqrt(ttl))) * snpd )
+          + r * K * exp(-r * ttl) * cdf(-d2) - d * S * exp(-d * ttl) * cdf(-d1));
+      break;
+    default:
+      result = -1;
+      break;
+  }
+
+  set_result(ret, result);
+  return 0;
+}
+
+static int greek_rho(option_data od, pricing_data pd, double S,
+  date ttl, result ret, void *pm_data) {
+  // exercise_type et = option_get_et(o);
+  /* check if it is eur... etcccc */
+
+  option_type type = od_get_option_type(od);
+  // date maturity = option_get_maturity(o); /* no use here */
+
+  double  sigma = pd_get_volatility(pd),
+          r = pd_get_risk_free_rate(pd),
+          d = pd_get_dividend(pd),
+          K = od_get_strike(od);
+
+  double d1, d2, result;
+
+  d1 = (log(S / K) + (r - d + pow(sigma, 2) / 2) * ttl) / (sigma * sqrt(ttl));
+  d2 = d1 - sigma * sqrt(ttl);
+
+  switch (type) {
+    case OPTION_CALL:
+      result =  K * ttl * exp(-r * ttl) * cdf(d2);
+      break;
+    case OPTION_PUT:
+      result = - K * ttl * exp(-r * ttl) * cdf(-d2);
+      break;
+    default:
+      result = -1;
+      break;
+  }
+
+  set_result(ret, result);
+  return 0;
+}
+
+static int greek_vega(option_data od, pricing_data pd, double S,
+  date ttl, result ret, void *pm_data) {
+  // exercise_type et = option_get_et(o);
+  /* check if it is eur... etcccc */
+
+  // date maturity = option_get_maturity(o); /* no use here */
+
+  double  sigma = pd_get_volatility(pd),
+          r = pd_get_risk_free_rate(pd),
+          d = pd_get_dividend(pd),
+          K = od_get_strike(od);
+
+  double d1, snpd, result;
+
+  d1 = (log(S / K) + (r - d + pow(sigma, 2) / 2) * ttl) / (sigma * sqrt(ttl));
+
+  snpd = (1 / sqrt(2 * M_PI)) * exp(-pow(d1, 2) / 2);
+
+  result = S * exp(-d * ttl) * sqrt(ttl) * snpd;
+
+  set_result(ret, result);
+  return 0;
+}
+
 pricing_method new_european_analytic(pricing_data pd) {
-  return new_pricing_method_(option_price, NULL, NULL, NULL, NULL, NULL, pd, NULL);
+  return new_pricing_method_(option_price, greek_delta, greek_gamma, greek_theta,
+    greek_rho, greek_vega, pd, NULL);
 }
