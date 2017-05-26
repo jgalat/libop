3f5d862e0a45257b209c99eafbb9fc6ac9169113
Jorge Galat <jrgglt@gmail.com>
2017-05-26 11:45:12 -0300

    added checkers for valid option_type and exercise_type

--- src/option_data.c
+++ src/option_data.c
@@ -8,7 +8,29 @@ struct option_data_ {
   double        strike; /***/
 };
 
+static int valid_option_type(option_type ot) {
+  switch (ot) {
+    case OPTION_CALL:
+    case OPTION_PUT:
+      return 1;
+    default:
+      return 0;
+  }
+}
+static int valid_exercise_type(exercise_type et) {
+  switch (et) {
+    case AM_EXERCISE:
+    case EU_EXERCISE:
+      return 1;
+    default:
+      return 0;
+  }
+}
+
 option_data new_option_data(option_type t, exercise_type e, date m, double strike) {
+  if (!valid_option_type(t) || !valid_exercise_type(e))
+    return NULL;
+
   option_data od = malloc(sizeof(struct option_data_));
   if (od) {
     od->type = t;
