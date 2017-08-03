94820ce1cf9952f87c826e523dbaf70003660825
Jorge Galat <jrgglt@gmail.com>
2017-08-03 11:01:41 -0300

    TODO: EXAMPLE FOLDER..

--- example.c
+++ example.c
@@ -7,20 +7,20 @@ int main(int argc, char const *argv[]) {
   double S = 21, strike = 20;
   volatility sigma = new_volatility(0.4);
   risk_free_rate r = new_risk_free_rate(0.09);
-  // dividend d = new_continuous_dividend(0.1);
+  dividend d = new_continuous_dividend(0.1);
   double option_value = 3.447474;
 
   time_period tp = new_time_period_365d();
 
   int iv = 0 , eur = 0;
 
-  dividend d = new_discrete_dividend();
-  int size_dd = 1;
-  int dates[2] = { 182 };
-  double ammounts[2] = { 7 };
-
-  div_disc_set_dates_(d, tp, size_dd, dates);
-  div_disc_set_ammounts_(d, size_dd, ammounts);
+  // dividend d = new_discrete_dividend();
+  // int size_dd = 1;
+  // int dates[2] = { 182 };
+  // double ammounts[2] = { 7 };
+  //
+  // div_disc_set_dates_(d, tp, size_dd, dates);
+  // div_disc_set_ammounts_(d, size_dd, ammounts);
 
   option opt;
   pricing_method pm;
